e6bc076cda0b66d61681b4b09735d627b8a90bf0
Stephen Fang <safang99@gmail.com>
2021-10-26 08:21:05 -0400

    final code

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_204727) do
+ActiveRecord::Schema.define(version: 2021_10_25_202245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "aliens", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
